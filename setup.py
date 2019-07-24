b899bedc97c8e87ec3fb53761faf3f87a3a84243
jingleman <jingleman12@gmail.com>
2019-07-24 14:46:12 -0700

    Minor improvements including makeing sure build is removed along with make clean.

--- setup.py
+++ setup.py
@@ -14,18 +14,15 @@ import os
 extra_compile_args = sysconfig.get_config_var('CPPFLAGS').split()
     # https://buildmedia.readthedocs.org/media/pdf/pythonextensionpatterns/latest/pythonextensionpatterns.pdf
     # hard to find this, and had to rerun many times for some reason.
-extra_compile_args += ["-Wall", "-Wextra", "-std=c++11"]
-	# Didn't seem to speed up the Visibility_Polygon contruction.
+extra_compile_args += ["-Wall", "-Wextra", "-std=c++11", "-lstdc++"]
 extra_compile_args += ["-g3", "-O0", "-DDEBUG=9", "-UNDEBUG"]
 	# Commenting this out didn't seem to speed up the Visibility_Polygon contruction.
 
-visilibity_src = os.environ['VISILIBITY_SRC']
+visilibity_src = os.getenv('VISILIBITY_SRC', default='')
 compute_visibility = Extension('compute_visibility',
                       sources=['compute_visibility.cpp', visilibity_src + '/visilibity.cpp'],
                       extra_compile_args=extra_compile_args)
   # Requires sym link or copy of visilibity.cpp
-
-print([numpy.get_include(), '${VISILIBITY_SRC}/'])
 
 setup(name='compute visibility',
       version='1.0',
